3f7ce2d58bd2cdd201aee42d3be8be7f322e2877
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-08 09:57:39 +0530

    feat: proper ws/server setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import path from 'path';
 import { config } from 'dotenv';
+import { Server } from 'ws';
+import { createServer } from 'http';
 
 config(); // Import environment variables
 const port = process.env.PORT || 8000;
 const clientFileLoc = path.join(__dirname, '../client/build');
 
 const app = express();
-app.listen(port, () => console.log(`Server started on port ${port}`));
+
+const server = createServer(app); // Create http server
+server.listen(port, () => console.log(`Server listening on port ${port}`));
 
 app.use(express.static(clientFileLoc));
 app.get('/', (req, res) => {
   res.sendFile(clientFileLoc);
+})
+
+const wss = new Server({
+  server
 })
